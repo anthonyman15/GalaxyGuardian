da88a07c0fe0eeef9b0cdaf44d4171f3425ca7f8
fairywizard15 <mankapui15@yahoo.com.hk>
2022-03-07 21:37:00 +0000

    bullet collider fixed, enemy health and bullet damage set

--- GalaxyGuardian/Assets/Bullet.cs
+++ GalaxyGuardian/Assets/Bullet.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int damage = 10;
     void OnTriggerEnter2D(Collider2D coll)
     {
-
-        if(coll.gameObject.tag == "Enemy")
+        if (coll.gameObject.tag == "Enemy")
         {
-            Destroy(coll.gameObject);
+            /*Destroy(coll.gameObject);*/
+            Enemy enemy = coll.GetComponent<Enemy>();
+            enemy.enemyHp = Mathf.Max(0, enemy.enemyHp - damage);
+            if(enemy.enemyHp == 0)
+            {
+                Destroy(coll.gameObject);
+            }
+
         }
         Destroy(gameObject);
-            /*Destroy(coll.gameObject);*/
     }
 }
     
